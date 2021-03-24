1950116826dbff9d402bf21d85b76c5f3d090dc0
Zach Wilson <zachwil99@gmail.com>
2021-03-24 10:53:06 -0600

    updated enemy class

--- forthequeen/game/enemy.py
+++ forthequeen/game/enemy.py
@@ -1,12 +1,36 @@
-from game.actor import Actor
+# from .actor import Actor
+import arcade
+import random
+import data.constants as constants
 
 
-class Enemy(Actor):
-    def __init__(self, health, attack_damage):
-        super().__init__(health, attack_damage)
+class Enemy(arcade.Sprite):
+    """
+    A enemy sprite
+    """
 
-        self.center_x = 100
-        self.center_y = 10
+    def __init__(self, enemy_name: str = 'enemyPlaceHolder'):
 
-    def _move(self):
-        self.center_y += 1
+
+        center_x = random.randint(0, 5)
+        center_y = 5
+
+        # enemyPlaceHolder = {'health': 1, 'attack_damage': 0, 'cost': 1, 'coordinates': None}
+        skeleton = {'health': 200, 'attack_damage': 20, 'coordinates': [center_x, center_y]}
+        ghost = {'health': 100, 'attack_damage': 10, 'coordinates': [center_x, center_y]}
+        slime = {'health': 50, 'attack_damage': 15, 'coordinates': [center_x, center_y]}
+
+        enemies = {'skeleton': skeleton, 'ghost': ghost, 'slime': slime}
+        enemy = enemies[enemy_name]
+
+        try:
+            super().__init__(filename=f"{constants.ASSETS_PATH}/{enemy_name}.png")
+        except FileNotFoundError:
+            super().__init__(filename=constants.ENEMY_IMAGE)
+        
+        self.health = enemy['health']
+        self.damage = enemy['attack_damage']
+    
+    def move(self):
+        self.center_y -= 1
+
